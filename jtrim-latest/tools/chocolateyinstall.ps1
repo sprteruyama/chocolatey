3f0decdceca0da60192cc1cfd0f76237cbe5c24c
terushu <terushu@yahoo.co.jp>
2021-06-04 05:59:12 +0900

    jtrim 1.53c

---
+++ jtrim-latest/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.woodybells.com/softs/jt153c.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'Jtrim*'
+  checksum      = '0a849084f9b8df0fe49cb13fa71e8badc12a8174fe373e4111dc9bb9f84b438d'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
